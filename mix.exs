50ac3b349785397f8068b51a467415dc79dec0a5
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2025-07-11 17:18:08 +0300

    add saladui dependency

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule Cognit.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # A collection of Live View components inspired by shadcn
+      {:salad_ui, "~> 1.0.0-beta.3", only: :dev}
     ]
   end
 end
