63a5db87af1d3c1c1e23e942b1e82d6acf32b6b6
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2026-02-20 17:21:39 +0200

    style(topbar): simplify trigger layout

--- lib/cognit/components/topbar.ex
+++ lib/cognit/components/topbar.ex
@@ -12,13 +12,13 @@ defmodule Cognit.Components.Topbar do
     ~H"""
     <div
       class={[
-        "h-16 sticky top-0 z-[1] bg-background border-b border-border px-6 py-2 flex gap-2 justify-end items-center",
+        "h-16 sticky top-0 z-[1] bg-background border-b border-border px-6 py-2 flex gap-2 items-center",
         @class
       ]}
       {@rest}
     >
-      <.sidebar_trigger is_mobile class="md:hidden mr-auto" />
-      <.sidebar_trigger class="hidden md:inline-flex mr-auto" />
+      <.sidebar_trigger is_mobile class="md:hidden" />
+      <.sidebar_trigger class="hidden md:inline-flex" />
 
       {render_slot(@inner_block)}
     </div>
