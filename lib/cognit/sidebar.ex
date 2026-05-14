fc1df45566a32e015b4de2809c5d8b58a4f38c08
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2026-05-14 17:12:31 +0300

    fix(sidebar): unblock clicks on first menu items in collapsed groups

--- lib/cognit/sidebar.ex
+++ lib/cognit/sidebar.ex
@@ -501,7 +501,7 @@ defmodule Cognit.Sidebar do
       class={
         classes([
           "duration-200 flex h-8 shrink-0 items-center rounded-md px-2 font-medium text-sidebar-foreground/70 outline-none ring-sidebar-ring transition-[margin,opacity] ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0 [&>.icon]:text-[16px] [&>.icon]:shrink-0 text-xs",
-          "group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0",
+          "group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0 group-data-[collapsible=icon]:pointer-events-none",
           @class
         ])
       }
